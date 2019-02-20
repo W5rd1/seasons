c7ff43c4ed1445360c3c25919bbbb702517aec20
michael eldred <mickyfin10@hotmail.com>
2019-02-20 10:29:44 +0000

    conditionals

--- src/index.js
+++ src/index.js
@@ -7,18 +7,27 @@ class App extends React.Component {
     constructor(props) {
         super(props);
 
-        this.state = { lat: null };
+        this.state = { lat: null, errorMessage: '' };
         window.navigator.geolocation.getCurrentPosition(
             position => {
                 this.setState({ lat: position.coords.latitude })
             },
-            err => console.log(err)
+            err => {
+                this.setState({ errorMessage: err.message})
+            }
         );
     }
     render() {     
-        return <div>Lattitude: {this.state.lat} </div>;
-    }
-}
+        if (this.state.lat && !this.state.errorMessage) {
+            return <div> lattitude: {this.state.lat} </div>
+        }
+
+        if (!this.state.lat && this.state.errorMessage) {
+            return <div> Error: {this.state.errorMessage} </div>
+        }
+        return <div>Loading</div>
+    };
+};
 ReactDOM.render (
     <App />,
     document.querySelector('#root')
