1c15d2f28c15b7b7c076132ff886db9d1a40c692
michael eldred <mickyfin10@hotmail.com>
2019-02-26 11:06:30 +0000

    Added conditionals for display

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SeasonDisplay from './SeasonDisplay';
 
 
 
@@ -19,7 +20,7 @@ class App extends React.Component {
     
     render() {     
         if (this.state.lat && !this.state.errorMessage) {
-            return <div> lattitude: {this.state.lat} </div>
+            return <SeasonDisplay lat={this.state.lat}/>
         }
 
         if (!this.state.lat && this.state.errorMessage) {
--- src/seasonDisplay.js
+++ src/seasonDisplay.js
@@ -1,6 +1,23 @@
 import React from 'react';
 
-const SeasonDisplay = () => {
-    return <div>SeasonDisplay</div>
-}
+const GetSeason = (lat, month) => {
+    if (month > 2 && month <9) {
+        return lat > 0 ? 'Summer' : 'Winter';
+    } else {
+        return lat > 0 ? 'Winter' : 'Summer';
+    }
+};
+
+const SeasonDisplay = props => {
+    const season = GetSeason(props.lat, new Date().getMonth());
+    const text = season === 'Winter' ? "Burrr it's cold" : "It's summer, let's hit the beach"
+    const icon = season  === 'Winter' ? 'snowflake' : 'sun';
+    return(
+        <div>
+            <i className={`${icon} icon`} />
+            <h1>{text}</h1>
+            <i className={`${icon} icon`} />
+        </div>
+    )
+};
 export default SeasonDisplay;
